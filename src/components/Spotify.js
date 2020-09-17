aced8386c415b8c1b9fbbdcb7ae196c251c06c1f
Sven Kirschbaum <sven@elite12.de>
2020-09-17 02:27:57 +0200

    COnform to Spotify embed code

--- src/components/Spotify.js
+++ src/components/Spotify.js
@@ -6,7 +6,7 @@ function Spotify() {
     return (
         <div className={"spotify"}>
             <iframe title="spotify" src="https://open.spotify.com/embed/playlist/7MYB7DNEGeG1aFzX2Iaer6"
-                    allow="encrypted-media"/>
+                    allow="encrypted-media" allowtransparency="true"/>
         </div>
     );
 }
