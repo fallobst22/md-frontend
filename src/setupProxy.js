66b6a0edeacd4c27ae716c89043d62465c904a62
Sven Kirschbaum <sven@kirschbaum.me>
2022-03-30 22:14:44 +0200

    :arrow_down: Update to react-scripts 5

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,9 @@
 // noinspection NpmUsedModulesInstalled
-const proxy = require('http-proxy-middleware');
+const {createProxyMiddleware} = require('http-proxy-middleware');
 
 module.exports = function (app) {
     //Proxy to local backend
-    app.use(proxy('/api', {target: 'http://localhost:8080'}));
+    app.use(createProxyMiddleware('/api', {target: 'http://localhost:8080'}));
     //Proxy to production backend
     //app.use(proxy('/api', {target: 'https://www.markus-dope.de', secure: true, changeOrigin: true}));
 };
