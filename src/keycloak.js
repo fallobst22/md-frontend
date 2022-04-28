e8388ab4306fb93ceb65b51d8034e7d257667e09
Sven Kirschbaum <sven@kirschbaum.me>
2022-04-28 04:04:56 +0200

    Update Keycloak URL

--- src/keycloak.js
+++ src/keycloak.js
@@ -3,7 +3,7 @@ import Keycloak from 'keycloak-js'
 // Setup Keycloak instance as needed
 // Pass initialization options as required or leave blank to load from 'keycloak.json'
 const keycloak = new Keycloak({
-    url: 'https://id.elite12.de/auth',
+    url: 'https://id.elite12.de',
     realm: 'elite12',
     clientId: 'markusdope-frontend'
 })
